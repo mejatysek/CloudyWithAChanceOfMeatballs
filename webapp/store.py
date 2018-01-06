ef595799affacee280372a0c3f0cb1dd4becea33
Matěj Mihal <mejatysek@gmail.com>
2018-01-07 00:58:57 +0100

    added app

---
+++ webapp/store.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import redis
+import pickle
+from hashids import Hashids
+
+__author__ = 'mejty'
+
+
+class Store(object):
+
+    def __init__(self, db=4):
+        self._redis = redis.StrictRedis(db=db)
+
+    def _prepare_key(self, prefix, key):
+        return "cloudy:{prefix}:{key}".format(prefix=prefix, key=key)
+
+    def _add_user_id(self, user_id):
+        self._redis.set(self._prepare_key(user_id, "exists"), 1)
+
+    def _add_user_id_message(self, user_id):
+        self._redis.set(self._prepare_key(user_id, "message"), 1)
+
+    def show_user_message(self, user_id):
+        result = self._redis.exists(self._prepare_key(user_id, "message"))
+        if result:
+            self._redis.delete(self._prepare_key(user_id, "message"))
+        return result
+
+    def get_new_id(self):
+        int_id = self._redis.incr(self._prepare_key("app", "id"))
+        hashids = Hashids(salt="BMb658SWMXmEqxpOXFwVFpHajuhwpqu7w3+BFfnK", alphabet="0123456789abcdefghijklmnopqrstuvwxyz")
+        user_id = hashids.encode(int_id)
+        self._add_user_id(user_id)
+        self._add_user_id_message(user_id)
+        return user_id
+
+    def id_exists(self, user_id):
+        return self._redis.exists(self._prepare_key(user_id, "exists"))
+
+    def add_user_city(self, user_id, city):
+        self._redis.hset(self._prepare_key(user_id, "cities"), city['Key'], pickle.dumps(city, protocol=4))
+
+    def del_user_city(self, user_id, location_key):
+        self._redis.hdel(self._prepare_key(user_id, "cities"), location_key)
+
+    def get_user_cities(self, user_id):
+        data = self._redis.hgetall(self._prepare_key(user_id, "cities"))
+        if data:
+            result = {k.decode("utf-8"): pickle.loads(v) for k, v in data.items()}
+            return result
+        else:
+            return {}
+
+    def cache_acu(self, method, arguments, data, ex=None):
+        key = "{method}:{arguments}".format(method=method, arguments=arguments)
+        self._redis.set(self._prepare_key("acuweather", key), pickle.dumps(data, protocol=4), ex=ex)
+
+    def get_cached_acu(self, method, arguments):
+        key = "{method}:{arguments}".format(method=method, arguments=arguments)
+        data = self._redis.get(self._prepare_key("acuweather", key))
+        return pickle.loads(data) if data else data
+
---
+++ webapp/webapp.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+from string import Template
+from decimal import Decimal
+
+from flask import Flask, render_template, redirect, request, jsonify, Response
+
+from store import Store
+from weatherApi import WeatherApi
+
+app = Flask(__name__)
+
+store = Store()
+acuAPI = WeatherApi(store)
+
+
+@app.route('/')
+def index():
+    user_id = request.args.get('userid', '', type=str)
+    if store.id_exists(user_id):
+        return redirect("/{user_id}/".format(user_id=user_id), code=302)
+    else:
+        return render_template('index.html', user_id=user_id)
+
+
+@app.route('/id/')
+def gen_id():
+    new_id = store.get_new_id()
+    return redirect("/{user_id}/".format(user_id=new_id), code=302)
+
+
+@app.route('/<user_id>/')
+def main(user_id):
+    if store.id_exists(user_id):
+        cities = store.get_user_cities(user_id)
+        forecasts = []
+        for k, v in cities.items():
+            forecasts.append({"id": k, "lat": v['GeoPosition']['Latitude'], "long": v['GeoPosition']['Longitude'], 'icon': "/icon/{k}/".format(k=k)})
+        return render_template("forecast.html", user_id=user_id, cities=cities, forecasts=forecasts, show_id=store.show_user_message(user_id))
+    else:
+        return redirect("/", code=302)
+
+
+@app.route('/city/search/')
+def search_city():
+    query = request.args.get('q', '', type=str)
+    if len(query) > 3:
+        result = acuAPI.search_city(query)
+        data = {"results": [{"id": v["Key"], "text": "{city}({country})".format(city=v['LocalizedName'], country=v['Country']['ID'])} for v in result]}
+        return jsonify(data)
+
+
+@app.route('/<user_id>/city/<location_key>/add/')
+def add_city(user_id, location_key):
+    if store.id_exists(user_id):
+        city = acuAPI.get_city(location_key)
+        store.add_user_city(user_id, city)
+        return redirect("/{user_id}/".format(user_id=user_id))
+    else:
+        redirect("/", code=302)
+
+
+@app.route('/<user_id>/city/<location_key>/del/')
+def del_city(user_id, location_key):
+    if store.id_exists(user_id):
+        store.del_user_city(user_id, location_key)
+        return redirect("/{user_id}/".format(user_id=user_id))
+    else:
+        redirect("/", code=302)
+
+
+@app.route('/icon/<location_key>/')
+def forecast_icon(location_key):
+    forecast = acuAPI.get_5_day_forecast(location_key)['DailyForecasts'][0]
+    icon_num = forecast['Day']['Icon']
+    with open(Path("static/icons/{num}.svg".format(num=icon_num))) as icon:
+        icon_data = Template(icon.read())
+        result = icon_data.substitute(min='{0: >2}'.format(str(round(Decimal(forecast['Temperature']['Minimum']['Value'])))), max=str(round(Decimal(forecast['Temperature']['Maximum']['Value']))))
+        return Response(response=result, status=200, mimetype="image/svg+xml")
+
+
+@app.route('/<user_id>/forecast/')
+def api_forecast(user_id):
+    if store.id_exists(user_id):
+        cities = store.get_user_cities(user_id)
+        result = []
+        for k, v in cities.items():
+            forecast = acuAPI.get_5_day_forecast(k)
+            forecast_icon_url = "/icon/{k}/".format(k=k)
+            forecast['DailyForecasts'] = forecast['DailyForecasts'][0]
+            forecast['Cloudy'] = {'Name': v['LocalizedName'], 'GeoPosition': v['GeoPosition'], 'ForecastIconUrl': forecast_icon_url}
+            result.append(forecast)
+        return jsonify(result)
+    else:
+        return Response("Bad id", status=404)
+
+
+
+if __name__ == '__main__':
+    app.run()
